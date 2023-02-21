f4352f3b12f7c79cdc9efa3dd55bce44daeb9413
mahutt <tommy.mahut@gmail.com>
2023-02-21 18:02:53 -0500

    more on E3

--- E3.js
+++ E3.js
@@ -1,11 +1,48 @@
 const numbers = [1, 2, 3, 4, 5];
+const mixedData = [4, 5, "3.0 birds", true, "birds2"];
 
 function addNumbers(numbers) {
     
     //return numbers.reduce;
     let sum = 0;
-    for (let number in numbers) {
-        sum += numbers[number];
+    for (let number of numbers) {
+        sum += number;
     }
     return sum;
 }
+
+function findMaxNumber() {
+   let greatest = Number.NEGATIVE_INFINITY;
+   for (let number of arguments) {
+    if (number > greatest) {
+        greatest = number;
+    }
+   }
+   return greatest;
+}
+
+function addOnlyNumbers(mixedData) {
+    let sum = 0;
+    for (let data of mixedData) {
+        if (parseFloat(data)) {
+            sum += parseFloat(data);
+        }
+    }
+    return sum;
+}
+
+// function getDigits(in) {
+//     let out = "";
+//     for (let letter of )
+// }
+
+function reverseString(stringIn) {
+    let stringOut = "";
+    for (let letter of stringIn) {
+        stringOut = letter + stringOut;
+    }
+    return stringOut;
+}
+
+
+
