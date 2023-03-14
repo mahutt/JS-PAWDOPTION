f24431d13e4d14b30e92f04b28098ca4976d9b32
mahutt <tommy.mahut@gmail.com>
2023-03-14 15:58:19 -0400

    E4 finalized

--- E4.js
+++ E4.js
@@ -3,29 +3,34 @@ const ITP_PRICE = 86.00;
 const AJ_PRICE = 55.00;
 
 document.getElementById("process-button").onclick = process;
+const inputs = document.getElementById("form").getElementsByTagName("input");
+const outputs = document.getElementById("display").getElementsByTagName("p");
 
 function process() {
-    let basic_web_prog_quantity  = parseInt(document.getElementById("BWP-q").value);
-    let intro_to_php_quantity    = parseInt(document.getElementById("ITP-q").value);
-    let advanced_jquery_quantity = parseInt(document.getElementById("AJ-q").value);
 
-    // document.getElementById("basic-web-prog-quantity").innerHTML =  " (Quantity = " + basic_web_prog_quantity  + "):";
-    // document.getElementById("intro-to-php-quantity").innerHTML =    " (Quantity = " + intro_to_php_quantity    + "):";
-    // document.getElementById("advanced-jquery-quantity").innerHTML = " (Quantity = " + advanced_jquery_quantity + "):";
+    for (let input of inputs) {
+        if (input.value == "" || /[^0-9]/.test(input.value)) {
+            alert("Please enter a valid number of books.")
+            input.focus();
+            return;
+        }
+    }
 
-    document.getElementById("BWP-subtotal").innerHTML = "<b>Basic Web Programming (Quantity = " + (basic_web_prog_quantity) + "):</b>";
-    document.getElementById("ITP-subtotal").innerHTML = "<b>Intro to PHP (Quantity = " + (intro_to_php_quantity) + "):</b>";
-    document.getElementById("AJ-subtotal" ).innerHTML = "<b>Advanced JQuery (Quantity = " + (advanced_jquery_quantity) + "):</b>";
+    let basic_web_prog_quantity  = parseInt(inputs["BWP-q"].value);
+    let intro_to_php_quantity    = parseInt(inputs["ITP-q"].value);
+    let advanced_jquery_quantity = parseInt(inputs["AJ-q" ].value);
 
-    BWP_subtotal = basic_web_prog_quantity * BWP_PRICE;
-    ITP_subtotal = intro_to_php_quantity * ITP_PRICE;
-    AJ_subtotal = advanced_jquery_quantity * AJ_PRICE;
+    outputs["BWP-subtotal"].innerHTML = "<b>Basic Web Programming (Quantity = " + (basic_web_prog_quantity) + "):</b>";
+    outputs["ITP-subtotal"].innerHTML = "<b>Intro to PHP (Quantity = " + (intro_to_php_quantity) + "):</b>";
+    outputs["AJ-subtotal" ].innerHTML = "<b>Advanced JQuery (Quantity = " + (advanced_jquery_quantity) + "):</b>";
 
-    document.getElementById("BWP-subtotal").innerHTML += " $" + (BWP_subtotal);
-    document.getElementById("ITP-subtotal").innerHTML += " $" + (ITP_subtotal);
-    document.getElementById("AJ-subtotal" ).innerHTML += " $" + (AJ_subtotal);
+    let BWP_subtotal = basic_web_prog_quantity * BWP_PRICE;
+    let ITP_subtotal = intro_to_php_quantity * ITP_PRICE;
+    let AJ_subtotal = advanced_jquery_quantity * AJ_PRICE;
 
-    document.getElementById("total" ).innerHTML = "<b>Final Total:</b> $" + (BWP_subtotal + ITP_subtotal + AJ_subtotal); 
+    outputs["BWP-subtotal"].innerHTML += " $" + (BWP_subtotal);
+    outputs["ITP-subtotal"].innerHTML += " $" + (ITP_subtotal);
+    outputs["AJ-subtotal" ].innerHTML += " $" + (AJ_subtotal);
 
-
+    outputs["total"].innerHTML = "<b>Final Total:</b> $" + (BWP_subtotal + ITP_subtotal + AJ_subtotal);
 }
